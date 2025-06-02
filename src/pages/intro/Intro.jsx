c4cfd957301a65597382f78ad2ee8d5abadbbdc7
Yassine <baadiyassine01@gmail.com>
2025-06-02 15:49:08 +0200

    commit

---
+++ src/pages/intro/Intro.jsx
@@ -0,0 +1,24 @@
+import Chen from '../../assets/img/professeur-chen.png'
+
+export default function Intro(){
+
+
+    return(
+
+        <>
+            <div className="containerIntro">
+                <div className='imgChen'>
+                    <img src={Chen} alt="" />
+                </div>
+
+                <div className="containerTxt">
+                    <div className='txt'>
+
+                    </div>
+
+                </div>
+            </div>
+        
+        </>
+    )
+}
